a435d0139a417c8e8c6cfdf2dbc044aff56cf4fd
Adam Bajguz <adam.bajguz@gmail.com>
2020-09-20 20:37:27 +0200

    Added IFallbackValuesStore and EnvironmentVariablesFallbackStore

---
+++ Typin/OptionFallback/EnvironmentVariablesFallbackStore.cs
@@ -0,0 +1,60 @@
+﻿namespace Typin.OptionFallback
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Environment variable fallback value store.
+    /// </summary>
+    public class EnvironmentVariablesFallbackStore : IFallbackValuesStore
+    {
+        private readonly IReadOnlyDictionary<string, string> _environmentVariables;
+
+        /// <summary>
+        /// Initializes an instance of <see cref="EnvironmentVariablesFallbackStore"/>.
+        /// </summary>
+        public EnvironmentVariablesFallbackStore(IReadOnlyDictionary<string, string> environmentVariables)
+        {
+            _environmentVariables = environmentVariables;
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<string> Keys => _environmentVariables.Keys;
+
+        /// <inheritdoc/>
+        public IEnumerable<string> Values => _environmentVariables.Values;
+
+        /// <inheritdoc/>
+        public int Count => _environmentVariables.Count;
+
+        /// <inheritdoc/>
+        public bool ContainsKey(string key)
+        {
+            return _environmentVariables.ContainsKey(key);
+        }
+
+        /// <inheritdoc/>
+        public string this[string key]
+        {
+            get => _environmentVariables[key];
+        }
+
+        /// <inheritdoc/>
+        public bool TryGetValue(string key, out string value)
+        {
+            return _environmentVariables.TryGetValue(key, out value);
+        }
+
+        /// <inheritdoc/>
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            return _environmentVariables.GetEnumerator();
+        }
+
+        /// <inheritdoc/>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (_environmentVariables as IEnumerable).GetEnumerator();
+        }
+    }
+}
---
+++ Typin/OptionFallback/IFallbackValuesStore.cs
@@ -0,0 +1,13 @@
+﻿namespace Typin.OptionFallback
+{
+    using System.Collections.Generic;
+    using Typin.Attributes;
+
+    /// <summary>
+    /// Fallback value store. Values stored in class implementing this interaface will be used as fallback if no option value is specified (<see cref="CommandOptionAttribute"/>).
+    /// </summary>
+    public interface IFallbackValuesStore : IReadOnlyDictionary<string, string>
+    {
+
+    }
+}
