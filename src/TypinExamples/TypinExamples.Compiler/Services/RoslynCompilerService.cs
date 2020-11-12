5622bbca68d51d00c58876e3ea503920594c7352
Adam Bajguz <adam.bajguz@gmail.com>
2020-11-12 17:43:50 +0100

    RoslynCompiler hotfix

--- src/TypinExamples/TypinExamples.Compiler/Services/RoslynCompilerService.cs
+++ src/TypinExamples/TypinExamples.Compiler/Services/RoslynCompilerService.cs
@@ -65,7 +65,7 @@
             {
                 Logger.LogInformation("Initializing compiler");
                 BlazorBoot response = await client.GetFromJsonAsync<BlazorBoot>("_framework/blazor.boot.json");
-                HttpResponseMessage[] assemblies = await Task.WhenAll(response.Resources.Assembly.Keys.Select(x => client.GetAsync("_framework/_bin/" + x)));
+                HttpResponseMessage[] assemblies = await Task.WhenAll(response.Resources.Assembly.Keys.Select(x => client.GetAsync("_framework/" + x)));
 
                 List<MetadataReference> references = new List<MetadataReference>(assemblies.Length);
                 foreach (HttpResponseMessage asm in assemblies)
