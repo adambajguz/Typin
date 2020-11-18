271061342c1f84cd57f4507cc2589e011021d2fa
Adam Bajguz <adam.bajguz@gmail.com>
2020-11-18 17:02:10 +0100

    Font changes

--- src/TypinExamples/TypinExamples/wwwroot/assets/js/xterm-interop.js
+++ src/TypinExamples/TypinExamples/wwwroot/assets/js/xterm-interop.js
@@ -8,6 +8,7 @@ xtermInterop.initialize = function (id) {
         xtermInterop.terminals.set(id, terminal);
 
         terminal.open(document.getElementById(id));
+
         shellprompt = '$ ';
         //terminal.on("data", (data) => {
         //    terminal.write(data);
