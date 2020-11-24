8ce8a0902dd24838b3b75f68758542741d0dd685
Adam Bajguz <adam.bajguz@gmail.com>
2020-11-24 18:51:05 +0100

    Removed WorkerTest component

--- src/TypinExamples/TypinExamples.Infrastructure.TypinWeb/Handlers/WorkerRunExampleHandler.cs
+++ src/TypinExamples/TypinExamples.Infrastructure.TypinWeb/Handlers/WorkerRunExampleHandler.cs
@@ -9,24 +9,6 @@
     using TypinExamples.Domain.Models.Workers;
     using TypinExamples.Infrastructure.TypinWeb.Console;
     using TypinExamples.Infrastructure.TypinWeb.Services;
-    public static class TaskExtensions
-    {
-        public static async Task<T> WaitOrCancel<T>(this Task<T> task, CancellationToken token)
-        {
-            token.ThrowIfCancellationRequested();
-            await Task.WhenAny(task, token.WhenCanceled());
-            token.ThrowIfCancellationRequested();
-
-            return await task;
-        }
-
-        public static Task WhenCanceled(this CancellationToken cancellationToken)
-        {
-            var tcs = new TaskCompletionSource<bool>();
-            cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
-            return tcs.Task;
-        }
-    }
 
     public class WorkerRunExampleHandler : IWorkerRequestHandler<RunExampleCommand>
     {
@@ -45,7 +27,7 @@
             _exampleInvoker.AttachConsole(webConsole);
             //_exampleInvoker.AttachLogger(LoggerDestination);
 
-            await _exampleInvoker.Run(request.WebProgramClass, request.Args, new Dictionary<string, string>()).WaitOrCancel(cancellationToken);
+            await _exampleInvoker.Run(request.WebProgramClass, request.Args, new Dictionary<string, string>());
 
             return new WorkerResult { Data = $"Processed by WorkerPingHandler {request.TerminalId}" };
         }
