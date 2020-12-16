b3bc519faa4dddb324c754ef37b406a290d6a4dc
Adam Bajguz <adam.bajguz@gmail.com>
2020-12-16 18:15:40 +0100

    Added example ZIP download

--- src/TypinExamples/TypinExamples.Application/Configurations/ExampleDescriptor.cs
+++ src/TypinExamples/TypinExamples.Application/Configurations/ExampleDescriptor.cs
@@ -6,8 +6,12 @@
         public string? Name { get; init; }
         public bool IsNew { get; init; }
         public string[]? Modes { get; init; }
+
+        public string? DownloadPath { get; init; }
+
         public string? ProgramClass { get; init; }
         public string? WebProgramClass { get; init; }
+
         public string? Description { get; init; }
         public string[]? QuickStart { get; init; }
         public string QuickStartText => QuickStart is null ? string.Empty : string.Join('\n', QuickStart);
