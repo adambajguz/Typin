46d7b42f9f77de9ef61145fb28e04db65c2f5887
Adam Bajguz <adam.bajguz@gmail.com>
2020-12-23 17:49:18 +0100

    Added file lists to appsettings.json

---
+++ src/TypinExamples/TypinExamples/wwwroot/assets/files/src/TypinExamples.Timer/Commands/LongRunningCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace TypinExamples.Timer.Commands
+{
+    using System.Threading.Tasks;
+    using Typin;
+    using Typin.Attributes;
+    using Typin.Console;
+
+    [Command("long", Description = "Command to simulate long runnig task by passing a ms delay.")]
+    public class LongRunningCommand : ICommand
+    {
+        [CommandParameter(0, Description = "Delay in ms.")]
+        public int? Time { get; set; }
+
+        public async ValueTask ExecuteAsync(IConsole console)
+        {
+            await console.Output.WriteLineAsync(nameof(LongRunningCommand));
+            await Task.Delay(Time ?? 0);
+        }
+    }
+}
---
+++ src/TypinExamples/TypinExamples/wwwroot/assets/files/src/TypinExamples.Timer/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace TypinExamples.Timer
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Typin;
+    using Typin.Directives;
+    using Typin.Modes;
+    using TypinExamples.Infrastructure.TypinWeb.Commands;
+    using TypinExamples.Timer.Middleware;
+    using TypinExamples.Timer.Repositories;
+
+    public class Startup : ICliStartup
+    {
+        public void Configure(CliApplicationBuilder app)
+        {
+            app.AddCommandsFromThisAssembly()
+               .AddCommand<PipelineCommand>()
+               .AddCommand<ServicesCommand>()
+               .AddDirectivesFromThisAssembly()
+               .AddDirective<PreviewDirective>()
+               .UseMiddleware<ExecutionTimingMiddleware>()
+               .UseInteractiveMode(options: (cfg) => cfg.IsAdvancedInputAvailable = false);
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IPerformanceLogsRepository, PerformanceLogsRepository>();
+        }
+    }
+}
