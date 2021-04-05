8ddf5aef75dcb2d486a1120ac7b53d6e39ab56f1
Adam Bajguz <adam.bajguz@gmail.com>
2021-04-05 20:08:47 +0200

    Typo fixes and middleware img update

--- src/Typin/Typin/Directives/BuiltInDirectives.cs
+++ src/Typin/Typin/Directives/BuiltInDirectives.cs
@@ -19,7 +19,7 @@
 
         /// <summary>
         /// If application runs in interactive mode (using the [interactive] directive), it is possible to execute multiple commands in one processes.
-        /// The application will ran in loop, constantly asking user for command input.
+        /// The application will run a in loop, constantly asking user for command input.
         /// This is useful for situations when it is necessary to execute multiple commands (since you don't have to constantly type dotnet ...).
         /// Furthermore, application context can be shared, which is useful when you have a db connection or startup takes very long.
         /// </summary>
