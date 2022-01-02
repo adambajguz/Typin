f89708f539c25b5da381298e181f12d090f409a1
Adam Bajguz <adam.bajguz@gmail.com>
2022-01-02 12:30:15 +0100

    Repository structure refactor

---
+++ Typin/src/Core/Typin.Core/Internal/Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Typin.Utilities.Extensions
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Text;
+
+    /// <summary>
+    /// <see cref="StringBuilder"/> extensions.
+    /// </summary>
+    internal static class StringBuilderExtensions
+    {
+        /// <summary>
+        /// Apppends <see cref="char"/> is builder is not empty.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [ExcludeFromCodeCoverage]
+        public static StringBuilder AppendIfNotEmpty(this StringBuilder builder, char value)
+        {
+            return builder.Length > 0 ? builder.Append(value) : builder;
+        }
+    }
+}
